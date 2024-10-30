0cf0cf6d1dabb59df3423a75e1e3b79362c45201
Hagura21 <154094522+Hagura21@users.noreply.github.com>
2024-10-30 12:32:35 +0300

    Add files via upload

---
+++ Cinema/Film.cs
@@ -0,0 +1,79 @@
+﻿namespace Cinema
+{
+    using Staff;
+
+    /// Класс Фильмы.
+    public sealed class Film : IEquatable<Film>
+    {
+        /// Инициализирует новый экземпляр класса <see cref="Film"/>.
+        /// <param name="title"> Название.</param>
+        /// <param name="genre"> Жанр фильма. </param>
+        /// <param name="synopsis"> Краткий обзор. </param>
+        /// <param name="ageRestriction"> Возрастное ограничение. </param>
+        /// <param name="duration"> Продолжительность фильма. </param>
+        /// <param name="director"> Режиссер. </param>
+        /// <param name="cast"> Актерский состав. </param>
+        public Film(
+            string title,
+            string genre,
+            string synopsis,
+            int ageRestriction,
+            int duration,
+            string director,
+            string cast)
+        {
+            this.FilmId = Guid.NewGuid();
+            this.Title = title ?? throw new ArgumentNullException(nameof(title));
+            this.Genre = genre ?? throw new ArgumentNullException(nameof(genre));
+            this.Synopsis = synopsis ?? throw new ArgumentNullException(nameof(synopsis));
+            this.AgeRestriction = ageRestriction;
+            this.Duration = duration;
+            this.Director = director ?? throw new ArgumentNullException(nameof(director));
+            this.Cast = cast ?? throw new ArgumentNullException(nameof(cast));
+        }
+
+        public Guid FilmId { get; }
+        public string Title { get; }
+        public string Genre { get; }
+        public string Synopsis { get; }
+        public int AgeRestriction { get; }
+        public int Duration { get; }
+        public string Director { get; }
+        public string Cast { get; }
+
+
+        public bool Equals(Film? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.Title == other.Title &&
+                   this.Genre == other.Genre &&
+                   this.Synopsis == other.Synopsis &&
+                   this.AgeRestriction == other.AgeRestriction &&
+                   this.Duration == other.Duration &&
+                   this.Director == other.Director && 
+                   this.Cast == other.Cast;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return this.Equals(obj as Film);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Title, this.Genre, this.Synopsis, this.AgeRestriction, this.Duration, this.Director, this.Cast);
+        }
+
+        public override string ToString() =>
+            $"{this.Title} {this.Genre} {this.Synopsis}{this.AgeRestriction} {this.Duration} {this.Director} {this.Cast}";
+    }
+}
---
+++ Cinema/Ticket.cs
@@ -0,0 +1,64 @@
+﻿namespace Cinema
+{
+    using Staff;
+    using System.Xml.Linq;
+
+    /// Класс Билет.
+    public sealed class Ticket : IEquatable<Ticket>
+    {
+        /// Инициализирует новый экземпляр класса <see cref="Ticket"/>.
+        /// <param name="session"> Сеанс.</param>
+        /// <param name="user"> Клиент.</param>
+        /// <param name="seat"> Место в зале.</param>
+        /// <param name="cost"> Цена билета.</param>
+        public Ticket(
+            Session session,
+            User user,
+            string seat,
+            decimal cost)
+        {
+            this.TicketId = Guid.NewGuid();
+            this.Session = session ?? throw new ArgumentNullException(nameof(session));
+            this.User = user ?? throw new ArgumentNullException(nameof(user));
+            this.Seat = seat ?? throw new ArgumentNullException(nameof(seat));
+            this.Cost = cost;
+        }
+
+        public Guid TicketId { get; }
+        public Session Session { get; }
+        public User User { get; }
+        public string Seat { get; }
+        public decimal Cost { get; }
+
+        public bool Equals(Ticket? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.Session.Equals(other.Session) &&
+                   this.User.Equals(other.User) &&
+                   this.Seat == other.Seat &&
+                   this.Cost == other.Cost;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return this.Equals(obj as Ticket);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Session, this.User, this.Seat, this.Cost);
+        }
+
+        public override string ToString() => 
+            $"Ticket for {this.User.UserName} - sear: {this.Seat}, Cost: {this.Cost:C}";
+    }
+}
