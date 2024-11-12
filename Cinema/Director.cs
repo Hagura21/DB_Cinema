e33165b108b4f4a92dc0ed32b33862604747ca49
Hagura21 <154094522+Hagura21@users.noreply.github.com>
2024-11-12 22:18:28 +0300

    Add files via upload

---
+++ Cinema/Director.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="Director.cs" company="Кирюшин Н.А.">
+// Copyright (c) Кирюшин Н.А.. All rights reserved.
+// </copyright>
+
+namespace Cinema
+{
+    public sealed class Director : Person
+    {
+        public Director(string name) : base(name)
+        {
+            this.Films = new HashSet<Film>();
+        }
+
+        public ISet<Film> Films { get; }
+
+        public void AddFilm(Film film)
+        {
+            if (film == null)
+            {
+                throw new ArgumentNullException(nameof(film));
+            }
+
+            this.Films.Add(film);
+        }
+    }
+}
